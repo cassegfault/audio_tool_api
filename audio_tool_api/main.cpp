bab81929977db6a82507f2885492bafcea6adf95
Chris Pauley <chrispauley92@gmail.com>
2018-12-01 08:48:36 -0500

    Initial Commit

---
+++ audio_tool_api/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  audio_tool_api
+//
+//  Created by Chris Pauley on 12/1/18.
+//  Copyright © 2018 Chris Pauley. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
